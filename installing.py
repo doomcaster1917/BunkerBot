c170156a2d4310273aed018c017fa22a9daddb1e
Doomcaster1917 <webtalestoday@gmail.com>
2022-09-21 16:51:03 +0300

    Delete vk_config.v2.json

--- installing.py
+++ installing.py
@@ -7,6 +7,7 @@ import configparser
 import json
 import re
 import asyncio
+import time
 
 config = configparser.ConfigParser()
 
@@ -22,7 +23,7 @@ raw_images = {
 async def make_config():
     group_token = input("Введите токен паблика")
     while True:
-        try:
+        # try:
 
             bot = Bot(token=group_token)
 
@@ -34,20 +35,23 @@ async def make_config():
                     print(part, data['elements'][0]["photo_id"])
                     for num, item in enumerate(data['elements']):
 
+                        print(images[num])
                         item["photo_id"] = re.sub('photo', '', await PhotoMessageUploader(bot.api).upload(images[num]))
 
                 with open(f"carousels/{part}.json", "w") as jsonFile:
                     json.dump(data, jsonFile)
 
-                config['GROUP']['Token'] = group_token
-                with open('config.ini', 'w') as configfile:
+            config['GROUP'] = {}
+            config['GROUP']['Token'] = group_token
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            break
 
-                    config.write(configfile)
-                break
-        except:
-            time.sleep(5)
-            print('Токен от группы невалидный. Возможно, в настройках группы для токена вы забыли что-то указать.'
-                  'Прочитайте инструкцию заново и попробуйте создать новый токен.')
+        # except Exception as err:
+        #     print(err)
+        #     time.sleep(5)
+        #     print('Токен от группы невалидный. Возможно, в настройках группы для токена вы забыли что-то указать.'
+        #           'Прочитайте инструкцию заново и попробуйте создать новый токен.')
 
 async def other_settings():
     while True:
@@ -59,26 +63,30 @@ async def other_settings():
             user_login = input("Введите логин. Чтобы отказаться, введите: Отказаться.")
             if user_login.lower() == 'Отказаться'.lower():
                 break
-            user_password = input("Введите id вашей страницы, на которую дополнительная страница будет слать уведомления."
-                                  "Чтобы отказаться, введите: Отказаться.")
+            user_password = input("Введите пароль. Чтобы отказаться, введите: Отказаться.")
+
             if user_password.lower() == 'Отказаться'.lower():
                 break
-            admin_id = input("Введите пароль. Чтобы отказаться, введите: Отказаться.")
+            admin_id = input("Введите id вашей страницы, на которую дополнительная страница будет слать уведомления."
+                             "Чтобы отказаться, введите: Отказаться.")
             if admin_id.lower() == 'Отказаться'.lower():
                 break
             try:
                 user_token = await UserAuth().get_token(user_login, user_password)
                 user = User(token=user_token)
                 await user.api.messages.send(peer_id=int(admin_id), message='Тестовое сообщение от бота',  random_id=random.randrange(100000000000000000))
+                config['USER'] = {}
                 config['USER']['Token'] = user_token
                 config['USER']['Login'] = user_login
                 config['USER']['Password'] = user_password
+                config['ADMIN'] = {}
                 config['ADMIN']['Admin_id'] = admin_id
                 with open('config.ini', 'w') as configfile:
                     config.write(configfile)
                 print('Авторизация прошла успешно.')
                 break
-            except:
+            except Exception as err:
+                print(err)
                 print('Логин или пароль неверные, либо боту не удалось написать вам, потому что у вас на главной странице закрыты сообщения.')
 
 async def main():
